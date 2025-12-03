b9d125a360b9f82837121771b25560e66a64a9b5
MoFritzZz <puchnermoritz100@gmail.com>
2025-12-03 11:05:03 +0100

    MenuButtton added

--- ChessVSC/java.js
+++ ChessVSC/java.js
@@ -1,7 +1,19 @@
-
 const darkBtn = document.querySelector('.darkMode');
 
 darkBtn.addEventListener('click', () => {
+    
     document.body.classList.toggle('dark-active');
+    if (localStorage.getItem('darkmode') === 'true')
+    {
+        document.getElementById("darkModetxt").textContent = "Dark Mode";
+    } else 
+    {
+        document.getElementById("darkModetxt").textContent = "White Mode";
+    }
+    const isDark = document.body.classList.contains('dark-active');
+    localStorage.setItem('darkmode', isDark);
 });
 
+if (localStorage.getItem('darkmode') === 'true') {
+    document.body.classList.add('dark-active');
+}
