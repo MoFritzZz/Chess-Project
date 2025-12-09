58b51425427d0f866096184bc037de3e6836abbc
MoFritzZz <puchnermoritz100@gmail.com>
2025-12-09 10:11:58 +0100

    Issue link a (menu button)

--- ChessVSC/java.js
+++ ChessVSC/java.js
@@ -1,19 +1,23 @@
 const darkBtn = document.querySelector('.darkMode');
+const darkText = document.getElementById("darkModetxt");
 
+// Event Listener
 darkBtn.addEventListener('click', () => {
-    
     document.body.classList.toggle('dark-active');
-    if (localStorage.getItem('darkmode') === 'true')
-    {
-        document.getElementById("darkModetxt").textContent = "Dark Mode";
-    } else 
-    {
-        document.getElementById("darkModetxt").textContent = "White Mode";
-    }
+    
+    // Aktuellen Zustand nach toggle prüfen
     const isDark = document.body.classList.contains('dark-active');
+    
+    // Text setzen
+    darkText.textContent = isDark ? "White Mode" : "Dark Mode";
+
+    // Zustand speichern
     localStorage.setItem('darkmode', isDark);
 });
 
 if (localStorage.getItem('darkmode') === 'true') {
     document.body.classList.add('dark-active');
+    darkText.textContent = "White Mode";
+} else {
+    darkText.textContent = "Dark Mode";
 }
